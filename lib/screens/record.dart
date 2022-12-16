92045e1139fcf26d5d9d18ddd8b636e07be30c34
Koorosh Karkehabadi <koorosh.k98@gmail.com>
2022-12-16 16:08:14 +0330

    add record button

--- lib/screens/record.dart
+++ lib/screens/record.dart
@@ -5,6 +5,41 @@ class Record extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("re");
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+    return Column(
+      children: [
+        const Expanded(child: Text("record")),
+        Container(
+          padding: const EdgeInsets.all(8.0),
+          decoration: const BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: Colors.deepPurpleAccent,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30.0),
+                topRight: Radius.circular(30.0)),
+          ),
+          height: h / 3,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 100,
+                width: 100,
+                child: IconButton(
+                  padding: EdgeInsets.zero,
+                  icon: const Icon(
+                    Icons.play_arrow,
+                    size: 90,
+                    color: Colors.green,
+                  ),
+                  onPressed: () {},
+                ),
+              )
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
